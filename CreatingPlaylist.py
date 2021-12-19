d1f48c25de00acd9728053bcd1c69425af4a8c0f
SKamarah <k98.saleem@gmail.com>
2021-12-19 02:57:11 -0500

    Corrected spacing issues

--- CreatingPlaylist.py
+++ CreatingPlaylist.py
@@ -4,6 +4,7 @@ import requests
 #Access Token that allows a user to use API
 SPOTIFY_CREATE_PLAYLISTS_URL = "https://api.spotify.com/v1/users/31bqvovehavga6adndylwwjchrxe/playlists"
 ACCESS_TOKEN = "BQC85ZWY-B2gHgxZh4DRUBHpscCPRbWXsjQ3OrOiLemNWL8QX3grGwi12GjXIj7EmXdfTIn07MUQsMRgVEDiEpLncj7DMc3gWbP5QmLQYoeaF9KMnSYm0Y895TjMHeu1UeXk3jGR_WYjj_N1l32TI3wg9kiPaCvsl-9pS1T2KIOougsDwc-pOx1T8rRLY3RNAW-NaYx0ShC75ENg2ZqINAUhfAntfe1J"
+
 #Creating playlist by making request out to endpoint
 def create_playlist(name ,public):
     response = requests.post(
